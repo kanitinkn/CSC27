e80d1621178afb0ec405caef586af459a977980a
DevBear <kanitin.kn@outlook.com>
2014-12-16 17:55:59 +0700

    Prototype
    first commit

---
+++ app/src/main/java/devbear/csc27/OrderActivity.java
@@ -0,0 +1,18 @@
+package devbear.csc27;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class OrderActivity extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_order, container, false);
+    }
+
+}
